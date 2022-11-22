920d96dc5684db086938cfcd0ecec4cb696416d8
Kahomy Martinez <kahomya@gmail.com>
2022-11-21 22:26:19 -0400

    Front CRUD Plantilla de Notificaciones

--- src/frontend/ServicesDeskUCAB/DTO/EtiquetaDTO.cs
+++ src/frontend/ServicesDeskUCAB/DTO/EtiquetaDTO.cs
@@ -5,7 +5,7 @@ namespace ServicesDeskUCAB.DTO
     public class EtiquetaDTO
     {
         public int id { get; set; }
-        [Required(ErrorMessage = "Ingrese un Tipo de Cargo")]
+        [Required(ErrorMessage = "Ingrese una Etiqueta")]
         [StringLength(50)]
         public string? nombre { get; set; }
 
