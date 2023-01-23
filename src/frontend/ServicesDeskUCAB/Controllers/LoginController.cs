cb33cc84ead302bd750915898be3cd039b537573
Carlos Hernández <soycarlosluis862@gmail.com>
2023-01-23 09:23:59 +0100

    registrar empleado

--- src/frontend/ServicesDeskUCAB/Controllers/LoginController.cs
+++ src/frontend/ServicesDeskUCAB/Controllers/LoginController.cs
@@ -22,7 +22,7 @@ namespace ServicesDeskUCAB.Controllers
             using (var httpClient = new HttpClient())
             {
                 StringContent content = new StringContent(JsonConvert.SerializeObject(usuario), Encoding.UTF8, "application/json");
- 
+
                 using (var response = await httpClient.PostAsync("https://localhost:7198/Usuario/Login", content))
                 {
                     var response2 = await response.Content.ReadAsStringAsync();
@@ -32,12 +32,12 @@ namespace ServicesDeskUCAB.Controllers
                     {
                         string stringDataRespuesta = json_respuesta["data"].ToString();
                         user = JsonConvert.DeserializeObject<LoginDTO>(stringDataRespuesta);
-                        HttpContext.Session.SetInt32("userid",user.id);
+                        HttpContext.Session.SetInt32("userid", user.id);
                         HttpContext.Session.SetString("email", user.Email);
                         HttpContext.Session.SetString("rol", user.Discriminator);
                         return RedirectToAction("GestionTickets", "Ticket");
                     }
-                     ViewBag.Error = json_respuesta["message"].ToString();
+                    ViewBag.Error = json_respuesta["message"].ToString();
 
                 }
             }
@@ -50,12 +50,12 @@ namespace ServicesDeskUCAB.Controllers
         {
             using (var httpClient = new HttpClient())
             {
-                 if (!ModelState.IsValid)
-                 {
+                if (!ModelState.IsValid)
+                {
                     return View();
-                }       
+                }
                 StringContent content = new StringContent(JsonConvert.SerializeObject(usuario), Encoding.UTF8, "application/json");
- 
+
                 using (var response = await httpClient.PostAsync("https://localhost:7198/Usuario/Registrar", content))
                 {
                     var response2 = await response.Content.ReadAsStringAsync();
@@ -63,28 +63,31 @@ namespace ServicesDeskUCAB.Controllers
                     Console.WriteLine(json_respuesta["success"].ToString());
                     if (json_respuesta["success"].ToString() == "True")
                     {
-                         return RedirectToAction("VerificarUsuario");
+                        return RedirectToAction("VerificarUsuario");
                     }
-                    if(json_respuesta["exception"].ToString().Contains("No se puede insertar una fila de clave duplicada")){
-                    ViewBag.Error ="Usuario duplicado" ;
-                    }else{
-                        ViewBag.Error = json_respuesta["message"].ToString() ;
+                    if (json_respuesta["exception"].ToString().Contains("No se puede insertar una fila de clave duplicada"))
+                    {
+                        ViewBag.Error = "Usuario duplicado";
                     }
-                    
+                    else
+                    {
+                        ViewBag.Error = json_respuesta["message"].ToString();
+                    }
+
 
                 }
             }
-           
+
             return View();
         }
-      
+
         public ViewResult VerificarUsuario() => View();
         [HttpPost]
         public async Task<IActionResult> VerificarUsuario(string token)
         {
             using (var httpClient = new HttpClient())
             {
-                using (var response = await httpClient.GetAsync("https://localhost:7198/Usuario/Verificar?token="+token))
+                using (var response = await httpClient.GetAsync("https://localhost:7198/Usuario/Verificar?token=" + token))
                 {
 
                     var response2 = await response.Content.ReadAsStringAsync();
@@ -92,136 +95,135 @@ namespace ServicesDeskUCAB.Controllers
                     Console.WriteLine(json_respuesta["success"].ToString());
                     if (json_respuesta["success"].ToString() == "True")
                     {
-                       return RedirectToAction("Index");
+                        return RedirectToAction("Index");
                     }
-                    
+
                     ViewBag.Error = json_respuesta["message"].ToString() + json_respuesta["exception"].ToString();
 
                 }
             }
-           
+
             return View();
         }
-       
+
         public ViewResult Olvidocontrasena() => View();
         [HttpPost]
         public async Task<IActionResult> Olvidocontrasena(string email)
         {
             using (var httpClient = new HttpClient())
             {
-                    
-                
-                using (var response = await httpClient.GetAsync("https://localhost:7198/Usuario/olvido-contrasena?email="+email))
+
+
+                using (var response = await httpClient.GetAsync("https://localhost:7198/Usuario/olvido-contrasena?email=" + email))
                 {
-                     var response2 = await response.Content.ReadAsStringAsync();
+                    var response2 = await response.Content.ReadAsStringAsync();
                     JObject json_respuesta = JObject.Parse(response2);
                     Console.WriteLine(json_respuesta["success"].ToString());
                     if (json_respuesta["success"].ToString() == "True")
                     {
-                         return RedirectToAction("ResetPassword");
+                        return RedirectToAction("ResetPassword");
                     }
-                    
-                 ViewBag.Error = json_respuesta["message"].ToString() + json_respuesta["exception"].ToString();
+
+                    ViewBag.Error = json_respuesta["message"].ToString() + json_respuesta["exception"].ToString();
 
 
                 }
             }
-           
+
             return View();
         }
-          public ViewResult ResetPassword() => View();
+        public ViewResult ResetPassword() => View();
         [HttpPost]
         public async Task<IActionResult> ResetPassword(ResetPasswordDTO usuario)
         {
-              if (!ModelState.IsValid)
-                 {
-                    return View();
-                }  
+            if (!ModelState.IsValid)
+            {
+                return View();
+            }
             using (var httpClient = new HttpClient())
             {
                 StringContent content = new StringContent(JsonConvert.SerializeObject(usuario), Encoding.UTF8, "application/json");
- 
+
                 using (var response = await httpClient.PostAsync("https://localhost:7198/Usuario/Reset-Password", content))
                 {
-                   var response2 = await response.Content.ReadAsStringAsync();
+                    var response2 = await response.Content.ReadAsStringAsync();
                     JObject json_respuesta = JObject.Parse(response2);
                     Console.WriteLine(json_respuesta["success"].ToString());
                     if (json_respuesta["success"].ToString() == "True")
                     {
-                         return RedirectToAction("Index");
+                        return RedirectToAction("Index");
                     }
-                    
-                ViewBag.Error = json_respuesta["message"].ToString() + json_respuesta["exception"].ToString();
+
+                    ViewBag.Error = json_respuesta["message"].ToString() + json_respuesta["exception"].ToString();
 
 
                 }
             }
-           
+
             return View();
         }
-    
 
-      public async Task<IActionResult> RegistrarEmpleado(){
-        List<DepartamentoDTO> listDep = new List<DepartamentoDTO>();
-        List<CargoDTO> listCargo = new List<CargoDTO>();
-         using (var httpClient = new HttpClient())
+
+        public async Task<IActionResult> RegistrarEmpleado()
+        {
+            List<GrupoDTO> listDep = new List<GrupoDTO>();
+            List<CargoDTO> listCargo = new List<CargoDTO>();
+            using (var httpClient = new HttpClient())
             {
-                using (var departamento = await httpClient.GetAsync("https://localhost:7198/Departamento/ConsultaDepartamentos"))
+                using (var departamento = await httpClient.GetAsync("https://localhost:7198/api/grupos"))
                 {
                     if (departamento.IsSuccessStatusCode)
                     {
-                    var cargos = await httpClient.GetAsync("https://localhost:7198/Cargo/ConsultaCargo");
-                      string apiResponse = await departamento.Content.ReadAsStringAsync();
-                      string apiResponse2 = await cargos.Content.ReadAsStringAsync();
-                      listDep = JsonConvert.DeserializeObject<List<DepartamentoDTO>>(apiResponse);
-                      listCargo = JsonConvert.DeserializeObject<List<CargoDTO>>(apiResponse2);
-                      dynamic mymodel = new ExpandoObject();
-                      mymodel.Departamentos = listDep;
-                      mymodel.Cargos = listCargo;
-                     return View(mymodel);
+                        var cargos = await httpClient.GetAsync("https://localhost:7198/Cargo/ConsultaCargo");
+                        string apiResponse = await departamento.Content.ReadAsStringAsync();
+                        string apiResponse2 = await cargos.Content.ReadAsStringAsync();
+                        JObject json_dgrupo = JObject.Parse(apiResponse);
+                        
+                        if (json_dgrupo["success"].ToString() == "True" )
+                        {
+                            listDep = JsonConvert.DeserializeObject<List<GrupoDTO>>(json_dgrupo["data"].ToString());
+                            listCargo = JsonConvert.DeserializeObject<List<CargoDTO>>(apiResponse2);
+
+                            dynamic mymodel = new ExpandoObject();
+                            mymodel.Departamentos = listDep;
+                            mymodel.Cargos = listCargo;
+                            return View(mymodel);
+                        }
+                       
                     }
-                    
-                   
-
+                     return View();
                 }
-          return View();
             }
+        }
 
-    }
- [HttpPost]
-     public async Task<IActionResult> RegistrarEmpleado(int tipou, int departamentos, int cargos, string email , string password, string confirmationpassword)
-        {
-             RegistroDTO usuario = new RegistroDTO();
-             usuario.Email = email;
-             usuario.Password= password;
-             usuario.confirmationpassword = confirmationpassword;
-            using (var httpClient = new HttpClient())
-            {
-                StringContent content = new StringContent(JsonConvert.SerializeObject(usuario), Encoding.UTF8, "application/json");
- 
-                using (var response = await httpClient.PostAsync("https://localhost:7198/Usuario/Registrar?cargoid="+cargos+"&tipousuario="+tipou+"&Departamentoid=" + departamentos, content))
+                [HttpPost]
+                public async Task<IActionResult> RegistrarEmpleado(RegistroDTO registro)
                 {
-                    if (response.IsSuccessStatusCode)
+
+                    using (var httpClient = new HttpClient())
                     {
-                         return RedirectToAction("VerificarUsuario");
+                        StringContent content = new StringContent(JsonConvert.SerializeObject(registro), Encoding.UTF8, "application/json");
+
+                        using (var response = await httpClient.PostAsync("https://localhost:7198/Usuario/Registrar", content))
+                        {
+                            if (response.IsSuccessStatusCode)
+                            {
+                                return RedirectToAction("VerificarUsuario");
+                            }
+
+                        }
                     }
-                    
-                    string apiResponse = await response.Content.ReadAsStringAsync();
-                    dynamic json  = JsonConvert.DeserializeObject(apiResponse);
-                    ViewBag.Result = json.errors;
 
+                    return View();
                 }
+
+
+                public IActionResult Cerrar()
+                {
+                    HttpContext.Session.Remove("userid");
+                    HttpContext.Session.Remove("email");
+                    return RedirectToAction("Index");
+                }
+
             }
-           
-            return View();
         }
-
-
-        public IActionResult Cerrar(){
-            HttpContext.Session.Remove("userid");
-            HttpContext.Session.Remove("email");
-            return RedirectToAction("Index");
-        }
-      
-    }
-}
